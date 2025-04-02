57c2ea0d18d8f3e5162da8c72fea0c46ea3caef5
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-01 22:22:04 -0400

    Added to include folder to ensure StringApp runs

---
+++ include/StringFunctions.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <utility>
+
+std::string reverseString(const std::string& input);
+bool isPalindrome(const std::string& input);
+std::pair<std::vector<char>, int> mostCommonChars(const std::string& input);
