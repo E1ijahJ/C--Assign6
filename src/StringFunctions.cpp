01a51dbab54c72aa8226e7581d33dd3eb1f05987
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-01 22:44:32 -0400

    Added documentation for all the funcitons

--- src/StringFunctions.cpp
+++ src/StringFunctions.cpp
@@ -4,7 +4,9 @@
 #include <utility>
 #include <algorithm>
 
-
+/// @brief Reverse the the inputed string 
+/// @param input the orginal string that was inputed 
+/// @return a nre string that is returned that is returned in reverse order
 std::string reverseString(const std::string& input) {
     std::string reversed = input;
     int left = 0;
@@ -18,7 +20,9 @@ std::string reverseString(const std::string& input) {
 
     return reversed;
 }
-
+/// @brief Sees if the inputed string is a palindrome, which is read the same forwards as it is a backward
+/// @param input the inputed string to chek
+/// @return it returns either true or false if it is a palindrome or not 
 bool isPalindrome(const std::string& input) {
     int left = 0;
     int right = input.length() - 1;
@@ -33,7 +37,9 @@ bool isPalindrome(const std::string& input) {
 
     return true;
 }
-
+/// @brief Finds the most common charachter in a string 
+/// @param input the inputed string 
+/// @return returns the charactersthat were the most common and then retursnthe number of times theu appeared 
 std::pair<std::vector<char>, int> mostCommonChars(const std::string& input) {
     std::unordered_map<char, int> freq;
     int maxCount = 0;
