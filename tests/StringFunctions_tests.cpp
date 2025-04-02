4c1b6a26c55a40bc86fef1549d0275b3aef1a3b8
E1ijahJ <elijahguijoseph@gmail.com>
2025-04-01 21:53:32 -0400

    Implemented testing for is a palindrome

--- tests/StringFunctions_tests.cpp
+++ tests/StringFunctions_tests.cpp
@@ -7,3 +7,14 @@ std::string reverseString(const std::string& input);
 TEST(ReverseStringTest, ReversesRegularString) {
     EXPECT_EQ(reverseString("hello"), "olleh");
 }
+
+
+bool isPalindrome(const std::string& input);
+
+TEST(IsPalindromeTest, DetectsPalindrome) {
+    EXPECT_TRUE(isPalindrome("racecar"));
+    EXPECT_TRUE(isPalindrome("madam"));
+    EXPECT_FALSE(isPalindrome("hello"));
+    EXPECT_FALSE(isPalindrome("abcde"));
+}
+
